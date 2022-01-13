c36c47146c440397934c972c84a06b6e4d4d621d
Gavin Lochtefeld <gavin.loch@gmail.com>
2022-01-13 13:16:15 -0600

    Normalized weapons further, also fixed some syntax errors

--- sql/Useful Queries/weapons.sql
+++ sql/Useful Queries/weapons.sql
@@ -1,19 +1,22 @@
 SELECT w.id,
 w.name AS [Weapon],
 w.cost || ' ' || LOWER(c.abbreviation) AS [Cost],
-w.dice || ' ' || dt.name AS [Damage],
+wd.dice || ' ' || dt.name AS [Damage],
 w.weight || ' lb.' AS [Weight],
 GROUP_CONCAT(
     wp.name 
     || CASE
-        WHEN wp.id = 3 OR wp.id = 10 THEN ' (range ' || wwp.lowerRange || '/' || wwp.upperRange || ')'
-        WHEN wp.id = 12 THEN ' (' || wwp.higherDice || ')'
+        WHEN wp.id = 3 OR wp.id = 10 THEN ' (range ' || wr.lowerRange || '/' || wr.upperRange || ')'
+        WHEN wp.id = 12 THEN ' (' || wd2.dice || ')'
         ELSE ''
         END, 
     ', ') AS [Properties]
 FROM weapon w
     INNER JOIN coin c ON w.coinID = c.id
+		INNER JOIN weaponDice wd ON w.diceID = wd.id
     INNER JOIN damageType dt ON w.damageTypeID = dt.id
     INNER JOIN weaponWithProperty wwp ON w.id = wwp.weaponID
+		INNER JOIN weaponRange wr on wwp.rangeID = wr.id
+		INNER JOIN weaponDice wd2 ON wwp.higherDiceID = wd2.id
     INNER JOIN weaponProperty wp ON wwp.propertyID = wp.id
 GROUP BY w.id;
--- sql/weapons.sql
+++ sql/weapons.sql
@@ -9,21 +9,32 @@ CREATE TABLE weaponProperty (
     description VARCHAR(1000) NOT NULL
 );
 
+CREATE TABLE weaponRange (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    lowerRange INTEGER,
+    upperRange INTEGER
+);
+
+CREATE TABLE weaponDice (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    dice VARCHAR(5) NOT NULL
+);
+
 CREATE TABLE weapon (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(25) NOT NULL,
     coinID REFERENCES coin (id) NOT NULL,
     cost INTEGER NOT NULL,
     damageTypeID INTEGER NOT NULL,
-    dice VARCHAR(5),
+    diceID REFERENCES weaponDice (id) NOT NULL,
     weight DECIMAL(18,10) NOT NULL,
     roll BLOB DEFAULT NULL
 );
 
+
 CREATE TABLE weaponWithProperty (
     weaponID REFERENCES weapon (id) NOT NULL,
     propertyID REFERENCES weaponProperty (id) NOT NULL,
-    lowerRange INTEGER DEFAULT NULL,
-    upperRange INTEGER DEFAULT NULL,
-    higherDice VARCHAR(5) DEFAULT NULL
+    rangeID REFERENCES weaponRange (id) NOT NULL,
+    higherDiceID REFERENCES weaponDice (id)
 );
