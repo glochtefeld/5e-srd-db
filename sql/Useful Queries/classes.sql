b6d78a28489fecab05bd600cac1b363cd9bbb04b
Gavin Lochtefeld <gavin.loch@gmail.com>
2022-01-26 15:20:33 -0600

    starting equipment queries done

--- sql/Useful Queries/classes.sql
+++ sql/Useful Queries/classes.sql
@@ -27,3 +27,31 @@ LEFT JOIN itemType tt on cttp.toolTypeID = tt.id
 LEFT JOIN classToolProficiency ctp on c.id = ctp.classID
 LEFT JOIN item t on ctp.toolID = t.id
 GROUP BY c.name
+
+create temp table if not exists starteq as
+select sei.id, sei.quantity,
+case
+	when sei.tableID=1 then a.name
+	when sei.tableID=2 then i.name
+	when sei.tableID=3 then it.name
+	when sei.tableID=4 then w.name
+	when sei.tableID=5 then wp.name
+	when sei.tableID=6 then ep.name
+	end as item
+from startEquipItem sei
+left join armor a on sei.itemID=a.id
+left join item i on sei.itemID=i.id
+left join itemType it on sei.itemID=it.id
+left join weapon w on sei.itemID=w.id
+left join weaponProperty wp on sei.itemID=wp.id
+left join equipmentPack ep on sei.itemID=ep.id;
+
+select seb.id, 
+sq.quantity || ' ' || sq.item || 
+case when sq2.item is not null then ', ' || sq2.quantity || ' ' || sq2.item else '' end ||
+case when sq3.item is not null then ', ' || sq3.quantity || ' ' || sq3.item else '' end 
+as [items]
+from startEquipBundle seb
+inner join starteq sq on seb.item1ID=sq.id
+left join starteq sq2 on seb.item2ID=sq2.id
+left join starteq sq3 on seb.item3ID=sq3.id;
